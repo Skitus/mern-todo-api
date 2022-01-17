18856a62c870b405083512255f58dce083b1f3ad
Skitus <artur.demenskiy03@gmail.com>
2022-01-17 11:28:19 +0200

    change test

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,6 @@ const app = express();
 const router = new AppRouter(app);
 connectDB();
 
-
 app.set("port", process.env.PORT || 5000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
